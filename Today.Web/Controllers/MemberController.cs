88bc8f52d5eee404863befb5f8ce2cd46dfb5371
JiaXuan <kitty881215@gmail.com>
2022-06-24 22:28:33 +0800

    建立controller

---
+++ Today.Web/Controllers/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Today.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult CountSetting()
+        {
+            return View();
+        }
+        public IActionResult Coupon()
+        {
+            return View();
+        }
+        public IActionResult OrderManage()
+        {
+            return View();
+        }
+        public IActionResult MessageManage()
+        {
+            return View();
+        }
+        public IActionResult MyCollect()
+        {
+            return View();
+        }
+        public IActionResult ShopCart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        
+
+    }
+}
---
+++ Today.Web/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Today.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult ProductPages() //商品頁面
+        {
+            return View();
+        }
+        public IActionResult Classify() //楊 分類
+        {
+            return View();
+        }
+       
+        public IActionResult Souvenir() //伴手禮
+        {
+            return View();
+        }
+        public IActionResult CityClassify() //城市 分類
+        {
+            return View();
+        }
+        public IActionResult CityTour() //各城市導覽頁
+        {
+            return View();
+        }
+        public IActionResult CityRaiders() //城市攻略
+        {
+            return View();
+        }
+        public IActionResult OffIsland() //離島 分類
+        {
+            return View();
+        }
+        public IActionResult ParentChild() //親子 分類
+        {
+            return View();
+        }
+        public IActionResult DIY() //DIY 分類
+        {
+            return View();
+        }
+        public IActionResult HSRClassify() //高鐵 分類
+        {
+            return View();
+        }
+        public IActionResult Rent() //租車 分類
+        {
+            return View();
+        }
+        public IActionResult Camping() //露營頁面
+        {
+            return View();
+        }
+        public IActionResult QuarantineHotel() //防疫旅館頁面
+        {
+            return View();
+        }
+        public IActionResult HSR() //國旅
+        {
+            return View();
+        }
+        public IActionResult AboutToday() //公司介紹
+        {
+            return View();
+        }
+    }
+}
