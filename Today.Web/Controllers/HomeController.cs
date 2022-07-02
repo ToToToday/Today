d86d2e2debfdc61781ca713dc05a181f61149786
JiaXuan <kitty881215@gmail.com>
2022-07-02 22:58:04 +0800

    update DB 20220702

--- Today.Web/Controllers/HomeController.cs
+++ Today.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Today.Model.Models;
+//using Today.Model.Models;
 using Today.Web.Models;
 
 namespace Today.Web.Controllers
---
+++ Today.Web/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Today.Web.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            InverseParentCategory = new HashSet<Category>();
+            ProductCategories = new HashSet<ProductCategory>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int? ParentCategoryId { get; set; }
+
+        public virtual Category ParentCategory { get; set; }
+        public virtual ICollection<Category> InverseParentCategory { get; set; }
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+    }
+}
