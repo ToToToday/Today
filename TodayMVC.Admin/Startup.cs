e1c94322d666becc0ee78de70f887a4a85f513b7
Eva <a0936804027@gmail.com>
2022-07-06 22:57:29 +0800

    2022/07/06

--- TodayMVC.Admin/Startup.cs
+++ TodayMVC.Admin/Startup.cs
@@ -58,7 +58,7 @@ namespace TodayMVC.Admin
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Data}/{id?}");
             });
         }
     }
