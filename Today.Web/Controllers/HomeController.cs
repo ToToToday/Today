ad08b5342200fdbf5a801cd3c6fef8c82dcb381e
二氧化硫 <yoyoball52099@gmail.com>
2022-07-17 00:17:57 +0800

    0

--- Today.Web/Controllers/HomeController.cs
+++ Today.Web/Controllers/HomeController.cs
@@ -39,7 +39,7 @@ namespace Today.Web.Controllers
                     Id = s.Id,
                     ProductPhoto = s.ProductPhoto,
                     ProductName = s.ProductName,
-                    Price = s.Prices.Price
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
                 Featured = productSource.Where(s => s.CityName.Contains("台北") || s.CityName.Contains("台南")).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -48,8 +48,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices == null || s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = (s.Prices == null) ? null : s.Prices.Price
+                    //OriginalPrice = (s.Prices == null || s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = (s.Prices == null) ? null : s.Prices.Price
                 }).Take(10).ToList(),
                 Paradise = productSource.Where(s => s.ChildCategoryName.Contains("樂園")).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -58,8 +58,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = s.Prices.Price
+                    //OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
                 AttractionTickets = productSource.Where(s => MaybeCategoryList(categorySource, "景點").Contains(s.ChildCategoryName)).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -68,8 +68,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = s.Prices.Price
+                    //OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
                 Exhibition = productSource.Where(s => MaybeCategoryList(categorySource, "展覽").Contains(s.ChildCategoryName)).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -78,8 +78,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = s.Prices.Price
+                    //OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
                 Hotel = productSource.Where(s => MaybeCategoryList(categorySource, "住宿").Contains(s.ChildCategoryName)).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -88,8 +88,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = s.Prices.Price
+                    //OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
                 Taoyuan = productSource.Where(s => s.CityName.Contains("桃園")).Select(s => new ProductVM.ProductCardInfo
                 {
@@ -98,8 +98,8 @@ namespace Today.Web.Controllers
                     ProductName = s.ProductName,
                     CityName = s.CityName,
                     Tags = s.Tags,
-                    OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
-                    Price = s.Prices.Price
+                    //OriginalPrice = (s.Prices.OriginalPrice == s.Prices.Price) ? null : s.Prices.OriginalPrice,
+                    //Price = s.Prices.Price
                 }).Take(10).ToList(),
             };
             
