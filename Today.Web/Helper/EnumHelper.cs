24e303b8469bb5eb116bc4b2e83919ee6e239c0e
JiaXuan <kitty881215@gmail.com>
2022-07-19 12:13:29 +0800

    Enum 修改

--- Today.Web/Helper/EnumHelper.cs
+++ Today.Web/Helper/EnumHelper.cs
@@ -17,10 +17,11 @@ namespace Today.Web.Helper
             return da.Description;
         }
         
+        
         public static string ToDescription<T>(this int enumValue) where T : Enum
         {
-            string str = enumValue.ToString();
-            var enumType = Enum.Parse(typeof(T), str);
+            var enumType = Enum.Parse(typeof(T), enumValue.ToString());
+            string str = enumType.ToString();
             System.Reflection.FieldInfo field = enumType.GetType().GetField(str);
             object[] objs = field.GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute), false);
             if (objs == null || objs.Length == 0) return str;
@@ -28,29 +29,6 @@ namespace Today.Web.Helper
             return da.Description;
         }
 
-        //public static string GetTypeDescription(int type)
-        //{
-        //    var enumType = Enum.Parse(typeof(CityEnum.PartnerType), type.ToString());
-        //    var result = GetDescription((Enum)enumType);
-        //    return result;
-        //}
-        //public static string GetDescription(Enum value)
-        //{
-        //    Type type = value.GetType();
-
-        //    //// 利用反射找出相對應的欄位
-        //    var field = type.GetField(value.ToString());
-        //    //// 取得欄位設定DescriptionAttribute的值
-        //    var customAttribute = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-        //    //// 無設定Description Attribute, 回傳Enum欄位名稱
-        //    if (customAttribute == null || customAttribute.Length == 0)
-        //    {
-        //        return value.ToString();
-        //    }
-
-        //    //// 回傳Description Attribute的設定
-        //    return ((DescriptionAttribute)customAttribute[0]).Description;
-        //}
+       
     }
 }
