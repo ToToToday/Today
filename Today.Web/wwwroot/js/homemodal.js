174692f2f150b96fd3ad24f0215fc89d75b4d2d7
Chihwen <chihwen1215@gmail.com>
2022-08-04 22:10:11 +0800

    0804盧

---
+++ Today.Web/wwwroot/js/homemodal.js
@@ -0,0 +1,29 @@
+﻿let submit = document.querySelector("#signupSubmit")
+let agreelabel = document.querySelector("#agree-label")
+let agreebox = agreelabel.querySelector("#checkAgree")
+let password = document.querySelector("#password")
+
+//agreelabel.addEventListener("click", function () {
+//    if (agreelabel.querySelector("#checkAgree").checked == true) {
+//        //console.log(agreelabel.querySelector("#checkAgree").checked)
+//        submit.disabled = false;
+//    }
+//    else {
+//        //console.log(agreelabel.querySelector("#checkAgree").checked)
+//        submit.disabled = true;
+//    }
+//})
+
+password.addEventListener('enter') //(未寫完)
+
+
+agreelabel.addEventListener("click", function () {
+    submit.disabled = true;
+    if (agreelabel.querySelector("#checkAgree").checked != true) {
+        //console.log(agreelabel.querySelector("#checkAgree").checked)
+        submit.disabled = true;
+    }
+    else {
+        submit.disabled = false;
+    }
+})
