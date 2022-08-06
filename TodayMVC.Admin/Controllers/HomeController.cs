4863780a9fec6a3c34ab7a36f16c659c25644560
JiaXuan <kitty881215@gmail.com>
2022-08-06 20:18:34 +0800

    update view admin xuan

--- TodayMVC.Admin/Controllers/HomeController.cs
+++ TodayMVC.Admin/Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace TodayMVC.Admin.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ TodayMVC.Admin/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodayMVC.Admin.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult MemberManage()
+        {
+            return View();
+        }
+    }
+}
