2b374a8bf1d708d9271ee690c4c2b3cc67a9db57
JiaXuan <kitty881215@gmail.com>
2022-08-09 21:39:23 +0800

    20220809 xuan

---
+++ TodayMVC.Admin/WebApiControllers/OrderApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodayMVC.Admin.WebApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderApiController : ControllerBase
+    {
+        public IActionResult AllOrder()
+        {
+            return Ok();
+        }
+    }
+}
