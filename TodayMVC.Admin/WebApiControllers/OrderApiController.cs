ee50ee673f742287292ff9694c5b95d19d36ebbc
JiaXuan <kitty881215@gmail.com>
2022-08-10 15:42:11 +0800

    20220810 XUAN

--- TodayMVC.Admin/WebApiControllers/OrderApiController.cs
+++ TodayMVC.Admin/WebApiControllers/OrderApiController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using Today.Model;
+using TodayMVC.Admin.Services.OrderServices;
+using TodayMVC.Admin.ViewModels;
 
 namespace TodayMVC.Admin.WebApiControllers
 {
@@ -7,9 +11,24 @@ namespace TodayMVC.Admin.WebApiControllers
     [ApiController]
     public class OrderApiController : ControllerBase
     {
-        public IActionResult AllOrder()
+        private readonly IOrderService _orderService;
+        public OrderApiController(IOrderService orderService)
         {
-            return Ok();
+            _orderService = orderService;
+        }
+        [HttpGet]
+        public IActionResult GetAllOrder()
+        {
+            try
+            {
+                var order = _orderService.GetAllOrder();
+                return Ok(new APIResult(APIStatus.Success, string.Empty, order));
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResult(APIStatus.Fail, ex.Message, null));
+            }
+            
         }
     }
 }
