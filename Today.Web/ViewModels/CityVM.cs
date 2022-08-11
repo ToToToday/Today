efed13ae646400ee31b9cc7b894b3de615145e52
二氧化硫 <yoyoball52099@gmail.com>
2022-08-11 21:44:50 +0800

    20220811yu

--- Today.Web/ViewModels/CityVM.cs
+++ Today.Web/ViewModels/CityVM.cs
@@ -33,7 +33,7 @@ namespace Today.Web.ViewModels
             public bool Favorite { get; set; }
             public string CityName { get; set; }
             public List<string> Tags { get; set; }
-            public float Rating { get; set; }
+            public double Rating { get; set; }
             public int TotalGiveComment { get; set; }
             public int TotalOrder { get; set; }
             public decimal? OriginalPrice { get; set; }
