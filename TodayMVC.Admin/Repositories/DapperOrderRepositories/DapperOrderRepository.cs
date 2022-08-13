8a6cf0ec8e2409af230499df2df534faa6a35d20
jiaxuan <kitty881215@gmail.com>
2022-08-14 04:51:27 +0800

    update url

--- TodayMVC.Admin/Repositories/DapperOrderRepositories/DapperOrderRepository.cs
+++ TodayMVC.Admin/Repositories/DapperOrderRepositories/DapperOrderRepository.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Today.Model.Models;
+using Slapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
 
 namespace TodayMVC.Admin.Repositories.DapperOrderRepositories
 {
@@ -30,26 +34,40 @@ namespace TodayMVC.Admin.Repositories.DapperOrderRepositories
         public IEnumerable<OrderDetail> SelectAll()
         {
 
-            var sql = @"SELECT o.OrderId,m.MemberName,p.ProductName,o.OrderDate,od.Quantity,od.UnitPrice,o.Status FROM[Order] o INNER JOIN OrderDetail od ON o.OrderId = od.OrderId INNER JOIN Member m ON o.MemberId = m.MemberId INNER JOIN ProgramSpecification s ON od.SpecificationId = s.SpecificationId INNER JOIN Program pg ON s.ProgramId = pg.ProgramId INNER JOIN Product p ON pg.ProductId = p.ProductId";
+            var sql = @"SELECT
+                         o.OrderId,
+                         m.MemberName,
+                         p.ProductName,
+                         o.OrderDate,
+                         od.Quantity,
+                         od.UnitPrice* od.Quantity AS Total,
+                         o.Status
+                        FROM[Order] o
+                        INNER JOIN OrderDetail od ON o.OrderId = od.OrderId
+                        INNER JOIN Member m ON o.MemberId = m.MemberId
+                        INNER JOIN ProgramSpecification s ON od.SpecificationId = s.SpecificationId
+                        INNER JOIN Program pg ON s.ProgramId = pg.ProgramId
+                        INNER JOIN Product p ON pg.ProductId = p.ProductId"; 
+        
+            var orderData = _conn.Query<dynamic>(sql);
+            StringBuilder sb = null;
+            foreach (dynamic item in orderData)
+            {
+                var d = item as IDictionary<string, object>;
+                if(sb == null)
+                {
+                    sb = new StringBuilder(string.Join("\t", d.Keys.ToArray()));
+                    sb.AppendLine();
+                }
+                sb.AppendLine(string.Join("\t", d.Keys.Select(n => d[n] == null ? string.Empty : d[n].ToString()).ToArray()));
+            }
+            
+            
+            return null;
+            //List<OrderDetail> orders = new List<OrderDetail>();
+            //var orderList = Slapper.AutoMapper.MapDynamic<OrderDetail>(orderData); /*as IEnumerable<OrderDetail>).ToList();*/
+            
 
-            //var orderData = _conn.Query<Order, OrderDetail, Member, Product, OrderTb>(sql, (order, od, m, p) =>
-
-            //{
-            //    return new OrderTb
-            //    {
-            //        OrderId = order.OrderId,
-            //        ProductName = p.ProductName,
-            //        OrderDate = order.OrderDate,
-            //        Quantity = od.Quantity,
-            //        Price = od.UnitPrice * od.Quantity,
-            //        Statu = order.Status
-            //    };
-            //},
-            //splitOn: "Id").ToList();
-
-
-            //return orderData;
-            return _conn.Query<OrderDetail>(sql);
             //var orderDictionary = new Dictionary<int, Order>();
             //var order = _conn.Query<Order, OrderDetail, Member, Product, Order>(sql, (order, od, m, p) =>
             //{
